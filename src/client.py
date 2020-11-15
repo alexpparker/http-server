3565d9bf02df89bd0c367d29cdee8f4e3e7afb8b
Alex Parker <alexpparker@gmail.com>
2020-11-15 16:33:37 +0000

    added keyboard interrupt handling

--- src/client.py
+++ src/client.py
@@ -1,10 +1,12 @@
 import socket
+import sys
 
 
 def client(message: str):
     addr_info = socket.getaddrinfo("127.0.0.1", 6000)
     stream_info = [socket_tup for socket_tup in addr_info if socket_tup[1] == socket.SOCK_STREAM][0]
     client_socket = socket.socket(*stream_info[:3])
+    print(client_socket)
     client_socket.connect(stream_info[-1])
     client_socket.sendall(message.encode("utf8"))
     print(client_socket.recv(64))
@@ -12,4 +14,4 @@ def client(message: str):
 
 
 if __name__ == "__main__":
-    client(input("Send your message\n"))
+    client(sys.argv[1])
