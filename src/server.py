933e9a7191f83d2d713c660cd29cf1c46df3e614
Alex Parker <alexpparker@gmail.com>
2021-01-01 16:34:39 +0000

    imported sys to server

--- src/server.py
+++ src/server.py
@@ -1,13 +1,14 @@
 import socket
+import sys
 
 
 def server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
     server_address = ("127.0.0.1", 6000)
     server_socket.bind(server_address)
+    server_socket.listen(1)
     try:
         while True:
-            server_socket.listen(1)
             connection, client_address = server_socket.accept()
             buffer_length = 9999
             received_message = connection.recv(buffer_length).decode("utf8")
@@ -15,7 +16,10 @@ def server():
             connection.close()
     except KeyboardInterrupt:
         server_socket.close()
+        print("\nShutting down the echo server.")
+        sys.exit()
 
 
 if __name__ == "__main__":
+    print("Your echo server is now running.")
     server()
